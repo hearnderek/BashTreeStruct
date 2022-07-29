4922e53b8bf3bfa87010959e11ad708499850991
Derek Hearn <hearnderek@yahoo.com>
2022-07-30 01:56:39 +0900

    switched to using int local & started cost calc

--- tree.sh
+++ tree.sh
@@ -1,99 +1,122 @@
 #!/bin/bash
 
 
-# adding int[] nodeindicies to structure
+# Ideas to use the tree
 # --------------------------------------
-# count_nodes:       uneffacted
-# append_node:       needs testing
-# find_node:         trivial but needs testing
-# append_child:      uneffected but was cleaned up so needs testing
-# get_child_indices: uneffected
-# shift:             needs testing
+# 1. dependency tree -- what kind?
+#
+# Task -> Dependent Task
+#
+#
+# 2. topological sort
 
 # How to represent a tree in bash
 
-# struct tree {           # Singleton
-#   int count             # fundamental for travercing the structure
-#   int[] nodeindicies    # count tells us how many indicies to jump to get past this
-#   node[] nodes          # beware, nodes vary in size
+# struct tree {          # Singleton
+#   int count            # fundamental for travercing the structure
+#   int[] nodeindices    # count tells us how many indices to jump to get past this
+#   node[] nodes         # beware, nodes vary in size
 # }
 
 # struct node {
-#   int childcount        # fundamental for knowing where structure ends
-#   int[] childindicies   # children must be within tree
+#   int childcount       # fundamental for knowing where structure ends
+#   int[] childindices   # children must be within tree
 # }
+debug="$1"
+
+if [ "$debug" == "debug" ]
+then
+  function trace() {
+    echo " > $@" 1>&2
+  }
+else
+  function trace() {
+    [ true ] # empty function
+  }
+fi
 
 # Store all nodes in a single array
 nodes=(0)
 
-function count_nodes(){
+function clear_nodes() {
+  nodes=(0)
+}
+
+function count_nodes() { # -> int count
   echo ${nodes[0]}
 }
 
-function append_node() {
-  echo " > append_node" 1>&2
+function append_node() { # -> int node index
+  trace "append_node"
+
   let newNodeIndex=`count_nodes`
   nodes+=(0)
-  let newNodePos=${#nodes[@]}        # Position of node header after a shift
-  let nodes[0]++                     # Increment count_nodes
-  let newNodeIndexRefPos=${nodes[0]} # Happens to be the exact location we need
-  shift $newNodeIndexRefPos          # Making room for new node index
+  local -i newNodePos=${#nodes[@]}        # Position of node header after a shift
+  let nodes[0]++                          # Increment count_nodes
+  local -i newNodeIndexRefPos=${nodes[0]} # Happens to be the exact location we need
+  shift $newNodeIndexRefPos               # Making room for new node index
   nodes[$newNodeIndexRefPos]=$newNodePos
+
+  echo $newNodeIndex                      # return
 }
 
-function find_node () {
-  echo " > find_node $1" 1>&2
+function find_node () { # -> int raw index
+  trace "find_node $1" >> /dev/null
   [ count_nodes == 0 ] && echo "No nodes to find" 1>&2 exit 01
-  let i=$1            # 0 indexed
-  let ip1=i+1         # skipping count
-  echo ${nodes[$ip1]} # pulling value from nodeindicies array
+  local -i i=$1         # 0 indexed
+  local -i ip1=i+1      # skipping count
+  echo ${nodes[$ip1]}   # pulling value from nodeindices array
 }
 
 function append_child() {
-  echo " > append_child $1 $2" 1>&2
-  let parentindex=$1                    # 0 indexed
-  let childindex=$2                     # 0 indexed; child is already added to tree
-  let nodecount=`count_nodes`
-  let parentPos=`find_node $parentindex`
+  trace "append_child $1 $2"
+  local -i parentindex=$1        # 0 indexed
+  local -i childindex=$2         # 0 indexed; child is already added to tree
+  local -i nodecount=`count_nodes`
+  local -i parentPos=`find_node $parentindex`
   
-  echo " >" appending child to node starting at $parentPos 1>&2
+  trace appending child to node starting at $parentPos
   let nodes[$parentPos]++   # increase node's childcount
   if [ $((nodecount + 1)) -eq $parentindex ] 
-  then                      # "the end"
-    nodes+=($childindex)    # the node substructure's childindicies are at end, and the meta structure has no meta data at the end
-  else                      # "in the middle"
-    let childindexpos=$parentPos+${nodes[$parentPos]}
+  then                      # -- the end of the raw array -- #
+    nodes+=($childindex)    # the node substructure's childindices are at end, and the meta structure has no meta data at the end
+  else                      # -- in the middle of the raw array -- #
+    local -i childindexpos=$parentPos+${nodes[$parentPos]}
     shift $childindexpos    # since we're adding an element in the middle of the structure we need to shift everything right
     let nodes[$childindexpos]=childindex
   fi
 }
 
-function get_child_indices() {
-  echo " > get_child_indices $1" 1>&2
-  let parentindex=$1
-  let parentPos=`find_node $parentindex`
-  let childcount=${nodes[$parentPos]} # childcount is first index of a node struct
+function add_parent() {
+  trace "add_parent $1 $2"
+  append_child $2 $1
+}
 
-  # Jump directly to start of parent node
-  echo " >" looking for parent at index $parentindex which was found at $parentPos 1>&2
+function get_child_indices() { # int[] node indices
+  trace "get_child_indices $1"
+  local -i parentindex=$1
+  local -i parentPos=`find_node $parentindex`
+  local -i childcount=${nodes[$parentPos]} # childcount is first index of a node struct
 
-  let i=1
+  trace looking for parent at index $parentindex which was found at $parentPos
+
+  local i=1                    # 1 indexed to make childPos calculation easier
   while [ $i -le $childcount ] # seq 1 $childcount
   do
-    let childPos=i+parentPos
+    local -i childPos=i+parentPos
     echo x $i at $childPos x 1>&2
-    echo ${nodes[$childPos]} # yeild return 
-    let i++
+    echo ${nodes[$childPos]}   # yeild return 
+    local -i i=$((i+1))
   done
 }
 
 function shift() {
-  echo " > shift $1" 1>&2
-  let pos=$1            # pure shell array index
+  trace "shift $1"
+  local -i pos=$1         # pure shell array index
   # -- Assertions -- #
   [ $pos -le 0 ] && echo "cannot shift count location" 1>&2 && exit 13
-  # --            -- #
-  let indexstart=1
+  # ---------------- #
+  local -i indexstart=1
   effectedNodes=()
   firstEffectedNodeIndex=
   for i in `seq 0 $((nodes[0] - 1))`
@@ -104,17 +127,30 @@ function shift() {
     fi
   done
 
-  let nodearraystart=${nodes[0]+1} 
-  let i=${#nodes[@]}    # count
-  let im1=i-1           # last index
-  while [ $i -gt $pos ] # seq $lastindex -1 $1
+  local -i nodearraystart=${nodes[0]+1} 
+  local -i i=${#nodes[@]}    # count
+  local -i im1=i-1           # last index
+  while [ $i -gt $pos ]      # seq $lastindex -1 $1
   do
     let nodes[$i]=${nodes[$im1]} # x[n] = x[n-1]
-    let i=im1   # == i--
-    let im1=i-1 # == im1--
+    local -i i=im1   # == i--
+    local -i im1=i-1 # == im1--
   done
 }
 
+function calculate_costs() {
+  local costs=
+  local 
+  for i in $( seq 1 $( count_nodes ) )
+  do
+    costs+=( 0 )
+    # dfs to find cost
+    echo $i
+  done
+  echo ${costs[@]}
+
+
+}
 
 # -- tests -- #
 
@@ -181,4 +217,6 @@ echo ${nodes[*]}
 echo "q: from 0 get children" `get_child_indices 0`
 echo q: from 1 get children `get_child_indices 1`
 echo q: from 2 get children `get_child_indices 2`
+
+calculate_costs
 
